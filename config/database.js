95fd2256e7ba390f99f033c61ba86934fbfce4c7
Pratham Thakkar <68506898+pratham0809@users.noreply.github.com>
2023-02-21 17:40:49 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoDb = "mongodb+srv://thakkarpratham20002:6RUGTglZBIUv3FrM@cluster0.2qji5uk.mongodb.net/?retryWrites=true&w=majority"
+const mongoDb = "mongodb+srv://<username>:<password.@cluster0.2qji5uk.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(mongoDb, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{console.log("Databse Connected!")})
